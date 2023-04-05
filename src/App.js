fd4b6c3afd3352ef24048b7a9b27f1fa573f8d43
mariyatom <mariyatom07@gmail.com>
2023-04-05 18:33:59 +1200

    imageupload react node work ,image path saving to db

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import AXIOS from 'axios';
 
 function App() {
+  const [image,setImage]= useState({preview:'',data:''});
+  const [status,setStatus]=useState('');
+  const handlerSubmit = async()=>{
+    let formdata = new FormData();
+    formdata.append('file',image.data)
+         await AXIOS.post("http://localhost:9000/imageup",formdata,{'context-type':'multipart/form-data'}).then((res)=>{
+          console.log(res.data.statusText);
+          setStatus(res.data.statusText);
+         })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>upload Image</h1>
+      {image.preview && <img src={image.preview} width="100" height="100" />}
+      <hr></hr>
+     <form onSubmit={handlerSubmit} encType='multipart/form-data'>
+      <input type='file'  name="file" onChange={(e)=>{
+          const img={
+            preview:URL.createObjectURL(e.target.files[0]),
+            data:e.target.files[0]
+
+
+          }
+          setImage(img);
+
+      }} />
+      <button type='submit'>upload file</button>
+      </form>
+      <h3>{status}</h3>
     </div>
   );
 }
